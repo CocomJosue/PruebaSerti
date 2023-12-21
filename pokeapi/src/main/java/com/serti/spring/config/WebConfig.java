b6490a5e40019e739310405b0a350f625d0a081e
RktSd98 <jacc11798@gmail.com>
2023-12-21 12:28:57 -0600

    creacion del proyecto y configuracion inicial

---
+++ pokeapi/src/main/java/com/serti/spring/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.serti.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.serti.spring.controller"})
+public class WebConfig  extends WebMvcConfigurerAdapter{
+	
+}
